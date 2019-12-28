b3a4901eceaf3e2ca7e8236ce4887d3f7c8ea8ba
xiegege <1522550956@qq.com>
2019-12-28 16:57:42 +0800

    建立项目

--- src/App.js
+++ src/App.js
@@ -3,22 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let title = ['1','2','3','4']
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        { 
+          title.map((v,i)=> <li key={i}>{v}</li>)
+        }
+      </ul>
     </div>
   );
 }
